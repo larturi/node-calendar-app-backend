ddcec34599a8e853dc4871e6faf342464df2c07b
lea.arturi <lea.arturi@gmail.com>
2021-02-25 14:46:45 -0300

    Front react a public v1

---
+++ public/precache-manifest.cd5d564d46bf811a666a9420c02bfc42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9822d9b2165b89b21387161968115229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8832392f20d7bf04dd10",
+    "url": "/static/css/2.1d09e8c2.chunk.css"
+  },
+  {
+    "revision": "456ce0150ba8aa7919f7",
+    "url": "/static/css/main.1d16e97b.chunk.css"
+  },
+  {
+    "revision": "8832392f20d7bf04dd10",
+    "url": "/static/js/2.983736d1.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.983736d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456ce0150ba8aa7919f7",
+    "url": "/static/js/main.ec336293.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
